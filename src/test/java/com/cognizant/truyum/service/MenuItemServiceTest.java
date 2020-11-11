040a61f50759e260449ce6fbe37b91e879fd0ce3
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2020-11-11 15:49:49 +0530

    Updated Menu Service Test

--- src/test/java/com/cognizant/truyum/service/MenuItemServiceTest.java
+++ src/test/java/com/cognizant/truyum/service/MenuItemServiceTest.java
@@ -4,10 +4,12 @@
 package com.cognizant.truyum.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -30,7 +32,7 @@ public class MenuItemServiceTest {
 	private MenuItemService menuItemService;
 
 	@Before
-	public void  initializeService() {
+	public void initializeService() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		// context.scan("com.cognizant.truyum");
 		// context.refresh();
@@ -56,12 +58,37 @@ public class MenuItemServiceTest {
 		assertTrue(result);
 
 	}
+
 	@Test
 	public void testGetMenuItemListCustomerSize() throws ClassNotFoundException, IOException, SQLException {
 
 		List<MenuItem> menuItemList = menuItemService.getMenuItemListCustomer();
-		
-		assertEquals(3,menuItemList.size());
-		
+
+		assertEquals(3, menuItemList.size());
+
 	}
+
+	@Test
+	public void testGetMenuItemListCustomerNotContainsFrenchFries()
+			throws ClassNotFoundException, IOException, SQLException {
+		List<MenuItem> menuList = menuItemService.getMenuItemListCustomer();
+		boolean result = false;
+		for (MenuItem m : menuList) {
+			if (m.getName().contentEquals("French Fries"))
+				result = true;
+		}
+		assertFalse(result);
+
+
+	}
+
+	@Test
+	public void testGetMenuItem() throws ClassNotFoundException, IOException, SQLException {
+
+		MenuItem menuItem = menuItemService.getMenuItem(1);
+		menuItem = menuItemService.getMenuItem(1);
+		assertTrue(menuItem.getName().equals("Sandwich"));
+
+	}
+
 }
